a351ea5e21efb16cc2054f94c75b4431d01d30bb
y0uf0ol <82054892+y0uf0ol@users.noreply.github.com>
2023-03-04 15:11:49 +0100

    inital testing

---
+++ backup_test.py
@@ -0,0 +1,17 @@
+import zipfile as zip
+import os
+from time import time, ctime
+
+backup_dst = '/Users/uwe/opt/Backup'
+t = time()
+Stamp = ctime(t)
+data = input('Enter the path to backup: ')
+
+
+with zip.ZipFile(Stamp+' backup.zip', 'w') as myzip:
+    for root, dirs, files in os.walk(data):
+        for file in files:
+            myzip.write(os.path.join(root, file))
+            print('File: ', file, ' added to archive')
+    print('Backup finished')
+
