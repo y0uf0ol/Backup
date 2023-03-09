a8f967ffe48980825908d536d835bac4c537198a
y0uf0ol <82054892+y0uf0ol@users.noreply.github.com>
2023-03-09 20:57:43 +0100

    added stuff
    compression ascii stuff and some fancy progress bars

--- backup_test.py
+++ backup_test.py
@@ -1,17 +1,43 @@
 import zipfile as zip
 import os
-from time import time, ctime
+from time import time, ctime, sleep
+import zlib
+from alive_progress import alive_bar
+
+print(r"""/
+▪       ▄▄ •       ▄▄▄▄▄     ▄· ▄▌      ▄• ▄▌    ▄▄▄▄· ▄▄▄        
+██     ▐█ ▀ ▪▪     •██      ▐█▪██▌▪     █▪██▌    ▐█ ▀█▪▀▄ █·▪     
+▐█·    ▄█ ▀█▄ ▄█▀▄  ▐█.▪    ▐█▌▐█▪ ▄█▀▄ █▌▐█▌    ▐█▀▀█▄▐▀▀▄  ▄█▀▄ 
+▐█▌    ▐█▄▪▐█▐█▌.▐▌ ▐█▌·     ▐█▀·.▐█▌.▐▌▐█▄█▌    ██▄▪▐█▐█•█▌▐█▌.▐▌
+▀▀▀    ·▀▀▀▀  ▀█▄▀▪ ▀▀▀       ▀ •  ▀█▄▀▪ ▀▀▀     ·▀▀▀▀ .▀  ▀ ▀█▄▀▪
+""")
 
 backup_dst = '/Users/uwe/opt/Backup'
 t = time()
 Stamp = ctime(t)
-data = input('Enter the path to backup: ')
+path_input = input('Enter the path to backup: ')
+Stamp_time=Stamp.replace(':', '_')
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
+def backup(data):
+    with zip.ZipFile(Stamp_time+' backup.zip', 'w', zip.ZIP_DEFLATED, compresslevel=9) as myzip:
+        for root, dirs, files in os.walk(data):
+                with alive_bar(len(files)) as bar:
+                    for file in files:
+                        myzip.write(os.path.join(root, file)) 
+                        bar()
+                        sleep(0.1)
+                        print('File: ', file, ' added to backup')
+                        sleep(0.1)  #feels more natural
+    print(bcolors.OKGREEN + 'Backup finished')
 
-with zip.ZipFile('backup.zip', 'w') as myzip:
-    for root, dirs, files in os.walk(data):
-        for file in files:
-            myzip.write(os.path.join(root, file))
-            print('File: ', file, ' added to archive')
-    print('Backup finished')
-
+backup(path_input)
